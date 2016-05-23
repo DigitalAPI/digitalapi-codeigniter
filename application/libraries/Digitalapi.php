5e09a576041a2386b06c1703f330a551bac99a04
ChandraBhanu Das <uxchandra@users.noreply.github.com>
2016-05-23 20:48:17 +0530

    Create Digitalapi.php

---
+++ application/libraries/Digitalapi.php
@@ -0,0 +1,7 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/**
+ *
+ * Class to utilize DigitalAPI  https://www.digitalapi.com/
+ * @author DigitalAPI.com
+ * @author DigtalAPI <info@digitalapi.com>
+ */
