401cca69455a362025bd011b1584458a9257dc92
ChandraBhanu Das <uxchandra@users.noreply.github.com>
2016-05-23 20:32:28 +0530

    Create digitalapi.php

---
+++ application/config/digitalapi.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Digital API custom config file.
+ */
+$config['api_key'] ='XXXXXXX PUT YOUR API KEY HERE XXXXXXXXX';
+$config['api_url'] ='https://www.digitalapi.com/api/' ;
+$config['api_version'] ='v1';
